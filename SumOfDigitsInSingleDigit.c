e1416f6b1815ea627d949e44d50e8b7773ce6cdc
MrPoudel <baalkissna2012@gmail.com>
2020-03-21 12:22:59 +0100

    Create SumOfDigitsInSingleDigit.c

---
+++ SumOfDigitsInSingleDigit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int digital_root(int n) {
+
+  int sum = 0;  
+  while( n > 0 || sum > 9)
+  {
+     if(n == 0)
+     {
+        n = sum;
+        sum = 0;
+     }     
+     sum += n % 10;
+     n /= 10;     
+   }   
+   return sum; 
+}
+
+int main()
+{
+	int result = digital_root(1234);
+  printf("\n%d\n", result);
+
+  return 0;
+}
