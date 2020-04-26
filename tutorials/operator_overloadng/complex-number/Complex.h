b63cf205c40a9bba08113ece31009fba62d93663
MrPoudel <baalkissna2012@gmail.com>
2020-04-27 01:10:44 +0200

    Inline get function  is made const.

--- tutorials/operator_overloadng/complex-number/Complex.h
+++ tutorials/operator_overloadng/complex-number/Complex.h
@@ -17,7 +17,7 @@ public:
 	Complex(const Complex& other);
 	const Complex& operator=(const Complex& other);
 	double getReal() const{ return real;}
-	double getImaginary(){ return imaginary;}
+	double getImaginary() const { return imaginary;}
 };
 
 ostream& operator<<(ostream& out, const Complex& other);
