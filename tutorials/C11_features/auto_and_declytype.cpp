58550aa77cf4ac471cc55d530f487d3f946c72b4
MrPoudel <baalkissna2012@gmail.com>
2020-04-27 22:11:16 +0200

    Auto and decltype demo is added.

---
+++ tutorials/C11_features/auto_and_declytype.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+template<class T, class S>
+auto test(T value1, S value2) -> decltype(value1+value2){  // This is called trailing return type.
+	return value1 + value2;
+}
+
+int get(){
+	return 999;
+}
+
+auto test2() -> decltype(get()){
+	return get();
+}
+
+int main() {
+
+	auto value = 7;
+	auto text = "Hello";
+
+	cout << value << endl;
+	cout << text << endl;
+	cout << test(5,6) << endl;
+	cout << test2() << endl;
+
+	cout << "Exited successfully!" << endl;
+
+	return 0;
+}
