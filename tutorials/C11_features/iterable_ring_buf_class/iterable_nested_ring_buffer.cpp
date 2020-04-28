2d58999b5e7d5d0d77da38d41dbf3264f1a4a2d3
MrPoudel <baalkissna2012@gmail.com>
2020-04-28 16:54:31 +0200

    Added  main test code.

--- tutorials/C11_features/iterable_ring_buf_class/iterable_nested_ring_buffer.cpp
+++ tutorials/C11_features/iterable_ring_buf_class/iterable_nested_ring_buffer.cpp
@@ -1,1 +1,44 @@
-//Empty
+#include <iostream>
+#include "ring.h"
+
+using namespace std;
+
+// Advance example in C++14 is here: https://codereview.stackexchange.com/questions/208293/ring-buffer-implementation-in-c14
+int main() {
+
+	ring<string> textstring(3);
+	textstring.add("one");
+	textstring.add("two");
+	textstring.add("three");
+//	textstring.add("four");
+
+	for(int i=0; i<textstring.size(); i++){
+		cout << textstring.get(i)<< endl;
+	}
+
+	cout << "Using user defined iterator.C++98 style" << endl;
+
+	for(ring<string>::iterator it = textstring.begin(); it != textstring.end(); it++){
+		cout << *it << endl;
+	}
+
+	cout << "Using C++11 syntax" << endl;
+	for(auto text: textstring){
+		cout << text << endl;
+	}
+
+//	/*
+//	 * Output should be like this:
+//	 * four
+//	 * one
+//	 * two
+//	 */
+//
+//	ring<int>::iterator it;
+//	it.print();
+
+
+	cout << "Exited successfully!" << endl;
+
+	return 0;
+}
