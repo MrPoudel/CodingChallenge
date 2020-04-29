fcb25b0327fcd97c91e76c2f1d5fc7488c2ebac2
MrPoudel <baalkissna2012@gmail.com>
2020-04-29 15:33:07 +0200

    Constructors and the memory -- deep copy of the objects.

---
+++ tutorials/C11_features/constructor_and_memory.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <memory.h> // Needed for memset
+
+using namespace std;
+
+class Test {
+private:
+	static const int SIZE=100;
+
+	int *_pBuffer;
+public:
+	Test(){
+		cout << "Constructor" << endl;
+		_pBuffer = new int[SIZE]{};
+		//_pBuffer = new int[SIZE]();
+		//memset(_pBuffer, 0, sizeof(int)*SIZE); //Can be avoided by () or {}
+	}
+
+	Test(int i) {
+		cout << "parameterized constructor" << endl;
+		_pBuffer = new int[SIZE]{};
+
+		for(int i = 0; i<SIZE; i++){
+			_pBuffer[i] = 7*i;
+		}
+	}
+
+	Test(const Test &other) {
+		cout << "Copy constructor" << endl;
+		_pBuffer = new int[SIZE]{};
+	}
+
+	Test &operator=(const Test &other){
+		cout << "assignment" << endl;
+		// Alternatively we could also call the copy constructor.
+		_pBuffer = new int[SIZE] {};
+
+		memcpy(_pBuffer, other._pBuffer, SIZE*sizeof(int));
+
+		return *this;
+	}
+
+	~Test(){
+		cout << "destructor" << endl;
+
+		delete [] _pBuffer;
+	}
+
+	friend ostream &operator<<(ostream &out, const Test &test);
+};
+
+ostream &operator<<(ostream &out, const Test &test){
+		out << "Hello from test" << endl;
+		return out;
+}
+
+Test getTest() {
+	return Test();
+}
+
+
+int main() {
+
+	Test test1 = getTest();
+	cout << test1 << endl;
+
+	// Test for vectors
+
+	cout << "Regarding vectors:" << endl;
+
+	vector<Test> vec;
+	vec.push_back(Test());
+
+	cout << "Exited successfully!" << endl;
+
+	return 0;
+}
+
+//Output : without : ‑fno‑elide‑constructors
+//Constructor
+//Hello from test
+//
+//Exited successfully!
+//destructor
+
+//**********************//
+//        : with     : ‑fno‑elide‑constructors
+//Constructor
+//Copy constructor
+//destructor
+//Copy constructor
+//destructor
+//Hello from test
+//
+//Exited successfully!
+//destructor
+
