b1eb6617a34df86e4b6e5ba806d2f699d276d6b2
MrPoudel <baalkissna2012@gmail.com>
2020-07-17 18:38:56 +0200

    Update FizzBuzz.c

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -10,3 +10,18 @@ void fizzBuzz(int n) {
             printf("%d\n", i);
     }
 }
+
+
+long findTheShortestRoof(vector<long> &cars, int k) {
+
+    sort(cars.begin(), cars.end());
+
+    long int min = LONG_MAX;
+    for(int i = 0; i <= (cars.size() - k); i++){
+        if((cars[i+k-1] - cars[i]) < min){
+            min = cars[i+k-1] - cars[i];
+        }
+    }
+
+    return min + 1;
+}
